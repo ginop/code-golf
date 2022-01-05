54f9ab2b0d40ca630da78109cf70aee3c843948b
Gino Perrotta <Gino.Perrotta@jhuapl.edu>
2022-01-04 22:50:32 -0500

    Enforce Black formatting. 828 characters

--- twelve_days_of_christmas.py
+++ twelve_days_of_christmas.py
@@ -1,3 +1,39 @@
-g=("twelve lords a-leaping,", "eleven ladies dancing,", "ten pipers piping,", "nine drummers drumming,", "eight maids a-milking,", "seven swans a-swimming,", "six geese a-laying,", "five golden rings,", "four calling birds,", "three French hens,", "two turtle-doves,", "and a partridge in a pear tree.")
-d=("first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth")
-song="\n\n".join(f"On the {d[n]} day of Christmas my true love gave to me\n"+(g[-1][4:] if n==0 else "\n".join([g[-2][:-1],g[-1]]) if n==1 else "\n".join(g[-(n+1):])) for n in range(12))
+g = (
+    "twelve lords a-leaping,",
+    "eleven ladies dancing,",
+    "ten pipers piping,",
+    "nine drummers drumming,",
+    "eight maids a-milking,",
+    "seven swans a-swimming,",
+    "six geese a-laying,",
+    "five golden rings,",
+    "four calling birds,",
+    "three French hens,",
+    "two turtle-doves,",
+    "and a partridge in a pear tree.",
+)
+d = (
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth",
+)
+song = "\n\n".join(
+    f"On the {d[n]} day of Christmas my true love gave to me\n"
+    + (
+        g[-1][4:]
+        if n == 0
+        else "\n".join([g[-2][:-1], g[-1]])
+        if n == 1
+        else "\n".join(g[-(n + 1) :])
+    )
+    for n in range(12)
+)
