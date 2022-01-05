c6cab400cb94008886ce091f3658683c81ad283f
Gino Perrotta <Gino.Perrotta@jhuapl.edu>
2022-01-04 22:13:45 -0500

    Initial commit of Twelve Days of Christmas code golf puzzle
    Baseline solution: write the entire text out manually. 2503 characters

---
+++ character_count.py
@@ -0,0 +1,9 @@
+
+def count_twelve_days():
+    with open("twelve_days_of_christmas.py") as file:
+        code = "\n".join([line for line in file])
+    print(f"Twelve Days of Christmas code has {len(code)} characters.")
+
+
+if __name__ == '__main__':
+    count_twelve_days()
